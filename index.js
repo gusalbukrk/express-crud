171e9dea6060e5bbc157a829ab09184279de1b33
gusalbukrk <gusalbukrk@gmail.com>
2023-07-24 21:34:46 -0300

    add `sequelize`

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 
 const app = express();
+
+// const sequelize = new Sequelize('postgres://postgres:pass1234@localhost:5433/world');
+//
+// constructor(database: string, username: string, password: string, options: object)
+const sequelize = new Sequelize('world', 'postgres', 'pass1234', {
+  host: 'localhost',
+  dialect: 'postgres',
+  port: 5433,
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database.');
+  // console.error(error);
+}
 
 app.get('/:resource/:id', async (req, res) => {
   const { resource, id } = req.params;
